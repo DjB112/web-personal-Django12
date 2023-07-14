9a6c64e6263045e9d4de32509280bb6179303c8e
DjB112 <david.j.bustamante@gmail.com>
2023-07-13 22:54:21 -0300

    13072023

--- webpersonal12/portfolio/admin.py
+++ webpersonal12/portfolio/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Project
 
 class ProyectAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
+    readonly_fields = ('created','updated')
 
 admin.site.register(Project, ProyectAdmin)
